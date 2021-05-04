6580d3058fff55413489e6cb5e366c6ac987b9b9
Rhet Turnbull <rturnbull+git@gmail.com>
2021-05-03 21:14:46 -0700

    Refactored database code

--- advent_bot.py
+++ advent_bot.py
@@ -1,3 +1,5 @@
+""" Twitter Bot to play Colossal Cave Adventure! """
+
 import logging
 import os
 import sqlite3
@@ -18,135 +20,20 @@ TWITTER_MAX_RESULTS = 20
 # TODO: move all game stuff to it's own Game class (that encapsulates an adventure.game.Game object)
 
 
-class AdventureBot:
-    def __init__(self):
-        logging.info(f"AdventureBot init")
+class AdventureDB:
+    """ Store game data """
 
-        self._api_key = os.environ["COLOSSAL_CAVE_API_KEY"]
-        self._api_secret = os.environ["COLOSSAL_CAVE_API_SECRET"]
-        self._access_token = os.environ["COLOSSAL_CAVE_ACCESS_TOKEN"]
-        self._access_secret = os.environ["COLOSSAL_CAVE_ACCESS_TOKEN_SECRET"]
-
-        # get twitter API instance
-        auth = tweepy.OAuthHandler(self._api_key, self._api_secret)
-        auth.set_access_token(self._access_token, self._access_secret)
-        self._api = tweepy.API(auth, wait_on_rate_limit=True)
-
-        self.database_name = DATABASE_NAME
-
-        self.max_results = TWITTER_MAX_RESULTS
-
-        try:
-            self._api.verify_credentials()
-            logging.info("Twitter authentication OK")
-        except:
-            raise ValueError("Error during twitter authentication")
-
+    def __init__(self, dbname):
+        self.database_name = dbname
         self.open_database()
-        self.state = self.load_state()
-        logging.info(f"self.state after loading {self.state}")
-        self.state = self.state or {
-            "last_seen_mention_id": 1389208292186988545,
-            "date": time.time(),
-        }
-
-    def get_mentions(self):
-        logging.info(f"Retrieving mentions")
-        since_id = self.state["last_seen_mention_id"]
-        logging.info(f"since_id: {since_id}")
-        for tweet in tweepy.Cursor(
-            self._api.search,
-            q=f"@{self._api.me().screen_name} -filter:retweets",
-            tweet_mode="extended",
-            since_id=since_id,
-            count=self.max_results,
-        ).items():
-            logging.info(f"tweet.id={tweet.id}, {since_id > tweet.id} {since_id - tweet.id}")
-            since_id = max(tweet.id, since_id)
-            self.state["last_seen_mention_id"] = since_id
-            logging.info(f"since_id: {since_id}")
-
-            if self.have_replied(tweet.id):
-                logging.info(f"Have already replied to tweet {tweet.id}")
-                continue
-
-            if tweet.display_text_range:
-                start, stop = tweet.display_text_range
-                text = tweet.full_text[start:stop]
-            else:
-                text = tweet.full_text
-
-            game = None
-            if tweet.in_reply_to_status_id is not None:
-                logging.info(
-                    f"in_reply_to_status_id: {tweet.in_reply_to_status_id}, id: {tweet.id}, user: {tweet.user.screen_name}"
-                )
-                logging.info(f"full_text: {tweet.full_text}, {text}")
-                game = self.load_game(tweet.in_reply_to_status_id)
-
-            if game:
-                logging.info(f"Loaded game from database, playing move")
-                self.play_move(tweet, text, game)
-            else:
-                # not a reply or didn't find game
-                logging.info(f"New game with {tweet.user.screen_name}")
-                self.new_game(tweet.id, screen_name=tweet.user.screen_name)
-
-        logging.info(f"since_id: {since_id}")
-        self.save_state()
-        return since_id
 
     def open_database(self):
         logging.info(f"Opening database {self.database_name}")
         self.db = sqlite3.connect(self.database_name)
         self._create_tables()
 
-    def play_move(self, tweet, text, game):
-        result = self.do_command(text, game)
-        logging.info(f"play_move result = '{result}'")
-        reply_tweet = self._api.update_status(
-            status=result,
-            in_reply_to_status_id=tweet.id,
-            auto_populate_reply_metadata=True,
-        )
-        self.save_game(
-            game, reply_tweet.id, tweet.id, result, screen_name=tweet.user.screen_name
-        )
-
-    def do_command(self, text, game):
-        command = text.lower().strip()
-        commands = command.split()
-        result = game.do_command(commands)
-        result = result.strip()
-        logging.info(f"result of do_command = {result}")
-        return result
-
-    def new_game(self, id_=None, screen_name=None):
-        game = Game()
-        load_advent_dat(game)
-        game.start()
-        # no instructions
-        result = game.do_command(["no"])
-        result = result.strip()
-        logging.info(f"result='{result}'")
-        tweet = None
-        if id_:
-            logging.info(f"Responding to id {id_}")
-            try:
-                tweet = self._api.update_status(
-                    status=result,
-                    in_reply_to_status_id=id_,
-                    auto_populate_reply_metadata=True,
-                )
-            except tweepy.error.TweepError as e:
-                logging.info(f"tweepy error: {e}")
-        else:
-            try:
-                tweet = self._api.update_status(status=result)
-            except tweepy.error.TweepError as e:
-                logging.info(f"tweepy error: {e}")
-        if tweet:
-            self.save_game(game, tweet.id, id_ or 0, result, screen_name=screen_name)
+    def close(self):
+        self.db.close()
 
     def save_game(self, game, tweet_id, reply_id, text, screen_name=None):
         c = self.db.cursor()
@@ -202,15 +89,15 @@ class AdventureBot:
         logging.info(f"Loaded state {state}")
         return state
 
-    def save_state(self):
+    def save_state(self, state):
         if not self.db:
             raise ValueError("database doesn't appear to be open")
 
-        logging.info(f"Saving state: {self.state}")
+        logging.info(f"Saving state: {state}")
         c = self.db.cursor()
         c.execute(
             "INSERT INTO state(last_seen_mention_id, date) values (?, ?);",
-            (str(self.state["last_seen_mention_id"]), time.time()),
+            (str(state["last_seen_mention_id"]), time.time()),
         )
         self.db.commit()
 
@@ -253,7 +140,137 @@ class AdventureBot:
 
     def __del__(self):
         if self.db:
-            self.save_state()
+            self.db.close()
+
+
+class AdventureBot:
+    def __init__(self):
+        logging.info(f"AdventureBot init")
+
+        self._api_key = os.environ["COLOSSAL_CAVE_API_KEY"]
+        self._api_secret = os.environ["COLOSSAL_CAVE_API_SECRET"]
+        self._access_token = os.environ["COLOSSAL_CAVE_ACCESS_TOKEN"]
+        self._access_secret = os.environ["COLOSSAL_CAVE_ACCESS_TOKEN_SECRET"]
+
+        # get twitter API instance
+        auth = tweepy.OAuthHandler(self._api_key, self._api_secret)
+        auth.set_access_token(self._access_token, self._access_secret)
+        self._api = tweepy.API(auth, wait_on_rate_limit=True)
+
+        self.max_results = TWITTER_MAX_RESULTS
+
+        try:
+            self._api.verify_credentials()
+            logging.info("Twitter authentication OK")
+        except:
+            raise ValueError("Error during twitter authentication")
+
+        self.db = AdventureDB(DATABASE_NAME)
+        self.state = self.db.load_state()
+        logging.info(f"self.state after loading {self.state}")
+        self.state = self.state or {
+            "last_seen_mention_id": 1389208292186988545,
+            "date": time.time(),
+        }
+
+    def get_mentions(self):
+        logging.info(f"Retrieving mentions")
+        since_id = self.state["last_seen_mention_id"]
+        logging.info(f"since_id: {since_id}")
+        for tweet in tweepy.Cursor(
+            self._api.search,
+            q=f"@{self._api.me().screen_name} -filter:retweets",
+            tweet_mode="extended",
+            since_id=since_id,
+            count=self.max_results,
+        ).items():
+            logging.info(
+                f"tweet.id={tweet.id}, {since_id > tweet.id} {since_id - tweet.id}"
+            )
+            since_id = max(tweet.id, since_id)
+            self.state["last_seen_mention_id"] = since_id
+            logging.info(f"since_id: {since_id}")
+
+            if self.have_replied(tweet.id):
+                logging.info(f"Have already replied to tweet {tweet.id}")
+                continue
+
+            if tweet.display_text_range:
+                start, stop = tweet.display_text_range
+                text = tweet.full_text[start:stop]
+            else:
+                text = tweet.full_text
+
+            game = None
+            if tweet.in_reply_to_status_id is not None:
+                logging.info(
+                    f"in_reply_to_status_id: {tweet.in_reply_to_status_id}, id: {tweet.id}, user: {tweet.user.screen_name}"
+                )
+                logging.info(f"full_text: {tweet.full_text}, {text}")
+                game = self.db.load_game(tweet.in_reply_to_status_id)
+
+            if game:
+                logging.info(f"Loaded game from database, playing move")
+                self.play_move(tweet, text, game)
+            else:
+                # not a reply or didn't find game
+                logging.info(f"New game with {tweet.user.screen_name}")
+                self.new_game(tweet.id, screen_name=tweet.user.screen_name)
+
+        logging.info(f"since_id: {since_id}")
+        self.db.save_state(self.state)
+        return since_id
+
+    def play_move(self, tweet, text, game):
+        result = self.do_command(text, game)
+        logging.info(f"play_move result = '{result}'")
+        reply_tweet = self._api.update_status(
+            status=result,
+            in_reply_to_status_id=tweet.id,
+            auto_populate_reply_metadata=True,
+        )
+        self.db.save_game(
+            game, reply_tweet.id, tweet.id, result, screen_name=tweet.user.screen_name
+        )
+
+    def do_command(self, text, game):
+        command = text.lower().strip()
+        commands = command.split()
+        result = game.do_command(commands)
+        result = result.strip()
+        logging.info(f"result of do_command = {result}")
+        return result
+
+    def new_game(self, id_=None, screen_name=None):
+        game = Game()
+        load_advent_dat(game)
+        game.start()
+        # no instructions
+        result = game.do_command(["no"])
+        result = result.strip()
+        logging.info(f"result='{result}'")
+        tweet = None
+        if id_:
+            logging.info(f"Responding to id {id_}")
+            try:
+                tweet = self._api.update_status(
+                    status=result,
+                    in_reply_to_status_id=id_,
+                    auto_populate_reply_metadata=True,
+                )
+            except tweepy.error.TweepError as e:
+                logging.info(f"tweepy error: {e}")
+        else:
+            try:
+                tweet = self._api.update_status(status=result)
+            except tweepy.error.TweepError as e:
+                logging.info(f"tweepy error: {e}")
+        if tweet:
+            self.db.save_game(game, tweet.id, id_ or 0, result, screen_name=screen_name)
+
+    def __del__(self):
+        if self.db:
+            self.db.save_state(self.state)
             logging.info(f"Closing database {self.db}")
             self.db.close()
 
