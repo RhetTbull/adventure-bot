5f7d0f4e8aff2d6f62622d5a65732674e7537eb1
Rhet Turnbull <rturnbull+git@gmail.com>
2021-05-06 22:42:04 -0700

    added --rm

--- docker_run.sh
+++ docker_run.sh
@@ -5,6 +5,7 @@
 docker run --name adventure-bot \
     --mount type=bind,source="$(pwd)"/shared,target=/shared \
     --env-file .env \
+    --rm \
     --restart on-failure \
     --log-opt mode=non-blocking \
     --log-opt max-buffer-size=1m \
